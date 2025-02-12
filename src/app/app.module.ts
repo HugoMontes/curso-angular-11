548f1a1e14109831b131ed78cc780a18bcceb5c8
Hugo Montes MMC <hugomontes927@gmail.com>
2025-02-11 20:22:16 -0400

    10. ListStaged > Commit 02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        RouterModule.forRoot([])
-    ],
-    providers: [],
-    bootstrap: [AppComponent],    
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot([])],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
