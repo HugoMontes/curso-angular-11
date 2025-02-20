f79fc72cb16d29d6e205b78405f43971d1aa26e0
Hugo Montes MMC <hugomontes927@gmail.com>
2025-02-20 16:30:26 -0400

    11. Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot([])],
-  providers: [],
+  imports: [BrowserModule, RouterModule.forRoot([]), MatButtonModule],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
