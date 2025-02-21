629ac889f4a9c412a4f78b986c3dd7be29421db3
Hugo Montes MMC <hugomontes927@gmail.com>
2025-02-20 20:48:41 -0400

    15. Crear un componente en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
+import { MyComponent1Component } from './my-component1/my-component1.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatButtonModule } from '@angular/material/button';
+import { MyComponent2Component } from './my-component2/my-component2.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyComponent1Component, MyComponent2Component],
   imports: [BrowserModule, RouterModule.forRoot([]), MatButtonModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
---
+++ src/app/my-component1/my-component1.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component1',
+  templateUrl: './my-component1.component.html',
+  styleUrl: './my-component1.component.scss',
+})
+export class MyComponent1Component {}
