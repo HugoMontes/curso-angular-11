b099a01bd306e713e4dbc2211d592ee0e52df712
Hugo Montes MMC <hugomontes927@gmail.com>
2025-02-25 17:50:38 -0400

    19. ngOnChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'curso-angular-11';
 
   constructor() {
     console.log('APP-COMPONENT >> CONSTRUCTOR');
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('APP-COMPONENT >> ngOnChanges');
+    console.log(changes);
+  }
+
   getMessage(value: string): void {
     console.log(value);
+  }
+
+  clickChangeTitle(): void {
+    this.title = 'CAMBIANDO.....';
   }
 }
--- src/app/my-component1/my-component1.component.ts
+++ src/app/my-component1/my-component1.component.ts
@@ -1,13 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-my-component1',
   templateUrl: './my-component1.component.html',
   styleUrl: './my-component1.component.scss',
 })
-export class MyComponent1Component {
+export class MyComponent1Component implements OnChanges {
   @Input() title: string | undefined;
   @Output() sendMessage = new EventEmitter<string>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('APP-COMPONENT-1 >> ngOnChanges');
+    console.log(changes);
+  }
 
   clickSentMessageToFather(): void {
     this.sendMessage.emit('Yo soy tu hijo');
