6fee6dbd7182ce16673cc552e09b1decb83c422e
Hugo Montes MMC <hugomontes927@gmail.com>
2025-02-24 20:45:43 -0400

    18. Constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'curso-angular-11';
 
+  constructor() {
+    console.log('APP-COMPONENT >> CONSTRUCTOR');
+  }
+
   getMessage(value: string): void {
     console.log(value);
   }
