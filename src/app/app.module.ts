b8b8743cf9487e758262c4b8720863f8afb3675f
hmontes <hugomontes927@gmail.com>
2025-08-12 14:48:08 -0400

    27. ngModule > Crear un modulo y exportar componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
-import { MyComponent1Component } from './my-component1/my-component1.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatButtonModule } from '@angular/material/button';
-import { MyComponent2Component } from './my-component2/my-component2.component';
+import { MyComponentsModule } from './components/my-components.module';
+// import { MyComponent1Component } from './components/my-component1/my-component1.component';
+// import { MyComponent2Component } from './components/my-component2/my-component2.component';
 
 @NgModule({
-  declarations: [AppComponent, MyComponent1Component, MyComponent2Component],
-  imports: [BrowserModule, RouterModule.forRoot([]), MatButtonModule],
+  declarations: [
+    AppComponent,
+    // MyComponent1Component,
+    // MyComponent2Component
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([]),
+    MatButtonModule,
+    MyComponentsModule,
+  ],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/my-component4/my-component4.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component4',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-component4.component.html',
+  styleUrl: './my-component4.component.scss',
+})
+export class MyComponent4Component {}
